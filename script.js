6b9590bcdd8971fa25bdbd50abd44d9354675a25
Damaris34 <oliveiradamares211@gmail.com>
2025-03-21 15:04:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,10 @@ let registros = [];
 function adicionarRegistro() {
     const data = document.getElementById('data').value;
     const consumo = document.getElementById('consumo').value;
+    const nomeColaborador = "Damaris"; // Pode ser dinâmico se necessário
 
     if (data && consumo) {
-        registros.push({ data, consumo });
+        registros.push({ data, nomeColaborador, consumo });
         atualizarTabela();
         limparFormulario();
     }
@@ -15,9 +16,12 @@ function atualizarTabela() {
     const tbody = document.querySelector('#registrosTable tbody');
     tbody.innerHTML = '';
     registros.forEach((registro, index) => {
+        const resultadoConsumo = index > 0 ? registro.consumo - registros[index - 1].consumo : 0;
         const row = `<tr>
             <td>${registro.data}</td>
+            <td>${registro.nomeColaborador}</td>
             <td>${registro.consumo}</td>
+            <td>${resultadoConsumo}</td>
         </tr>`;
         tbody.innerHTML += row;
     });
@@ -30,11 +34,12 @@ function limparFormulario() {
 
 function exportarParaExcel() {
     const csvRows = [];
-    const headers = ['Data', 'Consumo Diário'];
+    const headers = ['Data', 'Nome do Colaborador', 'Consumo Diário', 'Resultado do Consumo'];
     csvRows.push(headers.join(','));
 
-    registros.forEach(registro => {
-        const row = [registro.data, registro.consumo];
+    registros.forEach((registro, index) => {
+        const resultadoConsumo = index > 0 ? registro.consumo - registros[index - 1].consumo : 0;
+        const row = [registro.data, registro.nomeColaborador, registro.consumo, resultadoConsumo];
         csvRows.push(row.join(','));
     });
 
